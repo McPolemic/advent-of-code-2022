ea3be628e8f7eae457d1b0d12ad4f26acf3a4978
Adam Lukens <adam.lukens@mcpolemic.com>
2022-12-03 04:24:33 -0500

    Day 3 finished.

--- day3/day3.rb
+++ day3/day3.rb
@@ -8,6 +8,12 @@ def find_common_items(rucksack)
   pocket_1 & pocket_2
 end
 
+# Find the common item among three rucksacks
+def find_shared_badge(three_rucksacks)
+  items = three_rucksacks.map(&:chars)
+  items.reduce(:&)
+end
+
 # For each item, find its priority
 # 'a' .. 'z' = 1 .. 26
 # 'A' .. 'Z' = 27 .. 52
@@ -29,11 +35,14 @@ input = File.read('day3.txt')
   .lines(chomp: true)
 
 star_1 = input
-  .map do |rucksack|
-    priority(
-      find_common_items(rucksack)
-    )
-  end
+  .map { |rucksack| find_common_items(rucksack) }
+  .map { |items| priority(items) }
   .sum
 
+star_2 = input
+  .each_slice(3)
+  .flat_map { |three_rucksacks| find_shared_badge(three_rucksacks) }
+  .then { |badges| priority(badges) }
+
 puts "Star 1: #{star_1}"
+puts "Star 2: #{star_2}"
