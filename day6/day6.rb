fde28733b2ea68789f9dbce9fcde3618fecd37d8
Adam Lukens <adam.lukens@mcpolemic.com>
2022-12-06 02:56:02 -0500

    Day 6 Star 2

--- day6/day6.rb
+++ day6/day6.rb
@@ -4,7 +4,7 @@ def find_start_of_packet_marker(signal, packet_length = 4)
     .each_cons(packet_length)
     .with_index
     .select{ _1.first.uniq == _1.first }
-    .map{ [_1.first.join, _1.last + 4] }
+    .map{ [_1.first.join, _1.last + packet_length] }
     .first
 end
 
@@ -13,7 +13,7 @@ input = File.read('day6.txt')
   .first
 
 star_1 = find_start_of_packet_marker(input)
-star_2 = nil
+star_2 = find_start_of_packet_marker(input, 14)
 
 puts "Star 1: #{star_1}"
 puts "Star 2: #{star_2}"
